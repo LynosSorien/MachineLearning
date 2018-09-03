d5d117eaca24891ca5d9c689a2188e8631b7ad96
David Jorquera i Abellan <jorqueraabellandavid@gmail.com>
2018-09-03 16:14:45 +0200

    Enchanced lrm-descent and added feature scaling for new inputs

--- linear_regression/GradientDescent.py
+++ linear_regression/GradientDescent.py
@@ -7,6 +7,7 @@ class GradientDescent():
         self.alpha = alpha
         self.debug = debug
         self.min_diff = min_diff
+        self.first_theta = None
         if min_diff is None:
             self.min_diff = 0
 
@@ -17,24 +18,30 @@ class GradientDescent():
         self.training_values = training_values
         self.theta_values = [1] * (len(self.training_values)+1)
         self.T = np.matrix.transpose(self.training_features)
+        self.T = self.T.astype(float)
+        self.feature_scaling_coeficient = None
         if not all(number == 1 for number in self.T[0]):
             v = [1] * (len(self.T[0]))
             self.T = np.insert(self.T, 0, np.array(v), axis=0)
-            self.training_features = np.matrix.transpose(self.T)
+
+    def feature_scaling(self):
+        self.T, self.feature_scaling_coeficient = utils.feature_scaling(self.T)
+        self.training_features = np.matrix.transpose(self.T)
 
     def lrm_descent(self):
-        new_theta, cost_diff = self.cost_function()
-        #diff = abs(sum(new_theta)-sum(self.theta_values))
-        diff = abs(sum(cost_diff))
+        new_theta = self.theta_values
+        # diff = abs(sum(new_theta)-sum(self.theta_values))
+        diff = self.min_diff+1
         while diff > self.min_diff:
             self.theta_values = new_theta.copy()
             new_theta, cost_diff = self.cost_function()
-            #diff = abs(sum(new_theta) - sum(self.theta_values))
+            if self.first_theta is None:
+                self.first_theta = new_theta.copy()
+            # diff = abs(sum(new_theta) - sum(self.theta_values))
             diff = abs(sum(cost_diff))
             if self.debug:
                 print(new_theta)
-                print("Cost differences", cost_diff)
-
+                print("Difference", diff)
         self.theta_values = new_theta
         return self.theta_values
 
@@ -49,13 +56,26 @@ class GradientDescent():
 
     def use_function(self, vector):
         vector = np.insert(vector, 0, 1)
-        return utils.hypothesis(self.theta_values, vector)
+        new_vector = []
+        if self.feature_scaling_coeficient is not None:
+            for i in range(len(vector)):
+                new_vector.append(utils.feature_scaling_corrector(vector[i], self.feature_scaling_coeficient[i]))
+        else:
+            new_vector = vector
+        print(new_vector)
+        return utils.hypothesis(self.theta_values, new_vector)
 
 
 
-features = np.array([[5, 1, 2], [3, 5, 4], [3, 7, 6]])
-y = [2, 3, 4]
-alpha = 0.01
-gd = GradientDescent(alpha, features, y, 0, debug=True)
+features = np.array(
+    [
+        [5, 1500, 2],
+        [3, 5200, 3],
+        [3, 7001, 6]
+    ])
+
+y = np.array([2, 3, 4]).astype(float)
+alpha = 0.1
+gd = GradientDescent(alpha, features, y, min_diff=0, debug=True)
 gd.lrm_descent()
 ne_theta = utils.normal_equation(features, y)
--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
+from __future__ import division
 import numpy
+
+
 '''
     Solves the derivated cost_function of giving training set.
     The features of the training set must be minimized and normalized with
@@ -21,14 +24,15 @@ def cost_function(theta_vector, j, features, alpha, y_vector):
     return theta_j-alpha*(value/m)
 
 
+'''
+    hypothesis for linear regression multivariate. h(theta) = theta0 + theta1*x1 + ... + thetaN*xN
+    As we have x0 = 1 (always) we have two vectors of same dimension (n+1)x1. If transpose theta vector
+    we can obtain hypothesis value multiplying two vectors (as matrix operation). Because we have two matrix
+    of dimensions 1x(n+1) and (n+1)x1 we obtain a matrix with dimension 1x1, in other words, a scalar (hypothesis
+    value for h(theta_i).
+'''
 def hypothesis(theta_values, feature):
-    h = 0
-    for i in range(len(theta_values)):
-        theta = theta_values[i]
-        f = feature[i]
-        h += theta * f
-
-    return h
+    return numpy.matmul(numpy.transpose(theta_values), feature)
 
 
 def normal_equation(features, y_vector):
@@ -37,4 +41,31 @@ def normal_equation(features, y_vector):
     xt_x = numpy.matmul(xt, x)
     inverse = numpy.linalg.inv(xt_x)
     return numpy.matmul(numpy.matmul(inverse, xt), y_vector)
+
+
+def feature_scaling(t):
+    t = t.astype(float)
+    scaling_coef = []
+    for feature in t:
+        max_val = feature.max()
+        min_val = feature.min()
+        avg_val = sum(feature)/float(len(feature))
+        standard_dev = max_val-min_val
+        print("Feature =", feature, "max_val =", max_val, "min_val =", min_val, "avg_val =", avg_val)
+
+        for i in range(len(feature)):
+            if standard_dev == 0:
+                feature[i] = float(feature[i])/float(max_val)
+            else:
+                feature[i] = (float(feature[i])-avg_val)/float(standard_dev)
+
+        scaling_coef.append([avg_val, standard_dev, max_val])
+    return [t, scaling_coef]
+
+
+def feature_scaling_corrector(feature, corrector):
+    if corrector[1] == 0:
+        return float(feature)/float(corrector[2])
+    return (float(feature)-corrector[0])/float(corrector[1])
+
 
